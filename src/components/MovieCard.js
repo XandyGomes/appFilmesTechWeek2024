3e04c826213721ada2d987a3299a5239e71645ce
XandyGomes <x3.computadores@hotmail.com>
2024-05-20 21:37:10 -0300

    2024

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,65 +1,66 @@
-import {Text, View, StyleSheet, Image} from "react-native"
-import React from "react"
+import { Text, View, StyleSheet, Image } from "react-native";
+import React from "react";
 
-export default function MovieCard({ filme }){
-    const dataOriginal = new Date(filme.release_data)
-    const dataFormatada = new Intl.DateTimeFormat("pt-BR").format(dataOriginal)
+export default function MovieCard({ filme }) {
+  const dataOriginal = new Date(filme.release_data);
+  const dataFormatada = new Intl.DateTimeFormat("pt-BR").format(dataOriginal);
 
-    return(
-        <View style={styles.container}>
-            <Image
-            style={styles.image}
-            source={{ uri: `https://image.tmdb.org/t/p/w500${filme.poster_path}`  }}
-            />
-            <Text style={styles.title}>{ filme.title }</Text>
-            <Text style={styles.vote_avarage}> Nota: { filme.vote_avarage }</Text>
-            <Text style={styles.release_date}> Lançamento: { dataFormatada }</Text>
-            <Text numberOflines={1} style={styles.overview}> Sonopse: { filme.overview }</Text>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={{ uri: `https://image.tmdb.org/t/p/w500${filme.poster_path}` }}
+      />
+      <Text style={styles.title}>{filme.title}</Text>
+      <Text style={styles.vote_avarage}> Nota: {filme.vote_avarage}</Text>
+      <Text style={styles.release_date}> Lançamento: {dataFormatada}</Text>
+      <Text numberOflines={1} style={styles.overview}>
+        Sinopse: {filme.overview}
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: "#101217",
-      justifyContent: "center",
-      alignItems: "center",
-      borderRadius: 20,
-      padding: 20,
-      marginVertical: 10,
-      marginHorizontal: 16,
-      shadowColor: "#fff",
-      elevation: 5,
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: "bold",
-      textAlign: "center",
-      color: "#fff",
-    },
-    release_date: {
-      fontSize: 15,
-      color: "#999",
-    },
-    image: {
-      borderRadius: 10,
-      width: 130,
-      height: 200,
-      marginBottom: 10,
-    },
-    vote_average: {
-      borderTopColor: "#666",
-      borderTopWidth: 1,
-      borderBottomColor: "#666",
-      borderBottomWidth: 1,
-      padding: 5,
-      fontSize: 15,
-      color: "#999",
-    },
-    overview: {
-      fontSize: 15,
-      color: "#999",
-      textAlign: "center",
-    },
-  });
-  
+  container: {
+    backgroundColor: "#101217",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+    padding: 20,
+    marginVertical: 10,
+    marginHorizontal: 16,
+    shadowColor: "#fff",
+    elevation: 5,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#fff",
+  },
+  release_date: {
+    fontSize: 15,
+    color: "#999",
+  },
+  image: {
+    borderRadius: 10,
+    width: 130,
+    height: 200,
+    marginBottom: 10,
+  },
+  vote_average: {
+    borderTopColor: "#666",
+    borderTopWidth: 1,
+    borderBottomColor: "#666",
+    borderBottomWidth: 1,
+    padding: 5,
+    fontSize: 15,
+    color: "#999",
+  },
+  overview: {
+    fontSize: 15,
+    color: "#999",
+    textAlign: "center",
+  },
+});
