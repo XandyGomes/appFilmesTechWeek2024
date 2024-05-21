24aeb16e560bb39a5c0bde979b73eed6e6f546ed
XandyGomes <x3.computadores@hotmail.com>
2024-05-20 21:03:48 -0300

    2024

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'react-native';
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
